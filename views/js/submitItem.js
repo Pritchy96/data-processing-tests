0745ed5e8833aa6ba246d36cee1a7499ab5bf897
Tom Pritchard <tompritchard1996@gmail.com>
2017-02-18 01:27:26 +0000

    Now actually talks to server instead of localhost

--- views/js/submitItem.js
+++ views/js/submitItem.js
@@ -1,4 +1,4 @@
-//Triggers on clicking 'save' button, pushes content of item back to server.js
+//Triggers on clicking 'save' button, pushes content of item back to server.js where it is caught with a POST reqeust.
 $(document).ready(function(){
   var title, tags, content;
   $("#button-save-item").click(function(){
@@ -6,7 +6,7 @@ $(document).ready(function(){
     tags=$("#itemTags").val();
     content=$("#itemContent").val();
 
-    $.post("http://localhost:8080/addItem", {itemTitle: title, itemTags: tags, itemContent: content}, function(data){
+    $.post("http://52.39.116.224:8080/addItem", {itemTitle: title, itemTags: tags, itemContent: content}, function(data){
       if(data==='done')
         {
           alert("login success");
