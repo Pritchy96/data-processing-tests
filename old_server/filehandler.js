cd625a0ddbb8fd52995e6d3e79f191ed2b145b53
Tom Pritchard <tompritchard1996@gmail.com>
2017-06-23 00:01:17 +0100

    moved old server to own folder with dependancies

---
+++ old_server/filehandler.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+
+var filehandler = function (){
+   this.saveFile = function (node, callback){
+     var root = 'filestore';
+
+      if (!fs.existsSync(root)){
+          fs.mkdirSync(root);
+      }
+
+      //Path is the unique ID of the node in the databse.
+      var nodeFolder = root + "/" + node.nodeID;
+
+      if (!fs.existsSync(nodeFolder)){
+          fs.mkdirSync(nodeFolder);
+      }
+
+      //Handle file extensions here?
+      fs.writeFile(nodeFolder + "/" + node.version, node.content, function(err) {
+         if(err) {
+             return console.log(err);
+             //If there is an error here, we're in trouble, as the files data will
+             //already have been written to the database. Need some way to do tis synchronously,
+             //Or remove the node from database and present error here.
+         }
+
+         console.log("File saved.");
+         callback(null, node);
+     });
+   };
+
+   //Loads the content of an node from the filestore, given the nodes database entry.
+   this.loadFile = function (node, callback) {
+
+     if (node.node_ID == null || node.version == null) {
+       return console.error("node does not have id or version, cannot load")
+     }
+
+      var nodeLocation = "filestore/" + node.node_ID + "/" + node.version;
+
+      //Specify an encoding to return a string, or not to return an ASCII buffer.
+      fs.readFile( nodeLocation, { encoding: 'utf8' }, function (error, content) {
+        if (error) return console.error(error);
+        callback(null, content);
+      });
+   };
+};
+
+module.exports = filehandler;
---
+++ old_server/sql/mkTables.sql
@@ -0,0 +1,21 @@
+use data;
+
+CREATE TABLE nodes (
+  node_ID INT(11) NOT NULL AUTO_INCREMENT,
+  version INT(11),
+  #file_pointer CHAR(255) NOT NULL,
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  revision_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  delete_date   TIMESTAMP NULL,
+  PRIMARY KEY (node_ID)
+);
+
+CREATE TABLE tags (
+  tag_ID INT(11) NOT NULL AUTO_INCREMENT,
+  node_ID INT(11) NOT NULL,
+  `key` CHAR(100) NOT NULL,
+  `value` CHAR(100) NOT NULL,
+  UNIQUE KEY unique_tags (node_ID, `key`, `value`), #Ensures any given node doesn't have duplicate tags.
+  PRIMARY KEY (tag_ID),
+  FOREIGN KEY (node_ID) REFERENCES nodes(node_ID)
+);
